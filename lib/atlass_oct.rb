d88c09b841e286d9f8ac7e86e8b65a449e973391
kenken <kengoviolinphoto@gmail.com>
2023-11-30 23:23:01 +0900

    first commit

---
+++ lib/atlass_oct.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "atlass_oct/version"
+
+module AtlassOct
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "atlass_oct"
+
+require "minitest/autorun"
